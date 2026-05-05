9033735090fabe4523fbdabbc48bc5f9ed0ea185
ILMAN <ilmanalisher896@gmail.com>
2026-05-06 00:00:29 +0500

    change again to envs

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,10 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount  from "./serviceAccountKey.json"
 
-
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
